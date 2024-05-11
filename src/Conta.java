d13578180088d59f0785693bd9453f660c7ada8c
Kauan Meiguins <112512325+Meiguins@users.noreply.github.com>
2024-05-11 17:49:31 -0300

    add metodos IConta

--- src/Conta.java
+++ src/Conta.java
@@ -5,20 +5,15 @@ public class Conta implements IConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
-
-    public Conta(){
-        this.agencia = Conta.AGENCIA_PADRAO;
-        this.numero = SEQUENCIAL++;
-    }
-
+    
     @Override
     public void sacar(double valor){
-        saldo -= valor;
+
     }
 
     @Override
     public void depositar(double valor){
-        saldo += valor;
+
     }
 
     @Override
@@ -26,6 +21,10 @@ public class Conta implements IConta{
         
     }
 
+    public Conta(){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     public int getAgencia() {
         return this.agencia;
--- src/IConta.java
+++ src/IConta.java
@@ -1,3 +1,13 @@
 public interface IConta {
+    void sacar(double valor){
+        saldo -= valor;
+    }
 
+    void depositar(double valor){
+        saldo += valor;
+    }
+
+    void transferir(double valor, Conta contaDestino){
+
+    }
 }
