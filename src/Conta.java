18a1c12d45a040e219358d543dbe2a7d6bbb06b9
Kauan Meiguins <112512325+Meiguins@users.noreply.github.com>
2024-05-13 17:04:18 -0300

    Alterações nas classes

--- src/Conta.java
+++ src/Conta.java
@@ -5,7 +5,8 @@ public abstract class Conta implements IConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
-    
+    protected Cliente cliente;
+
     @Override
     public void sacar(double valor){
         saldo -= valor;
@@ -28,9 +29,10 @@ public abstract class Conta implements IConta{
         System.out.println(String.format("Saldo %.2f", this.saldo));
     }
 
-    public Conta(){
+    public Conta(Cliente cliente){
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
     }
 
     public int getAgencia() {
